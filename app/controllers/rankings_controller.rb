05723d16da826db25e8df3065e782bff56bf1f21
ayane.mitsuishi <ayane.mitsuishi@techacademy.jp>
2018-07-19 00:18:00 +0000

    ranking have[

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,11 +1,11 @@
 class RankingsController < ApplicationController
+  def have
+    @ranking_counts = Have.ranking
+    @items = Item.find(@ranking_counts.keys)
+  end
+  
   def want
     @ranking_counts = Want.ranking
-    #→ [3=>10, 5=>6, 2=>5, 8=>3, ...] のように [item_id => Want の数] というハッシュデータが入っています
-    #このデータだけではitemsを表示できないので、
     @items = Item.find(@ranking_counts.keys)
-                      #→ハッシュからitem_idだけ取り出した配列を取得
-            #→itemsを取得
-    #→ランキングの順番通りのまま
   end
 end
